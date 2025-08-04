48924d5780f0dafaf4f52d2e403aa9bdf338f55d
Shantanu-prajapati <159682238+Shantanu-prajapati@users.noreply.github.com>
2025-08-05 00:41:27 +0530

    .

---
+++ src/main/java/in/shani/billingsoftware/service/CategoryService.java
@@ -0,0 +1,17 @@
+package in.shani.billingsoftware.service;
+
+import in.shani.billingsoftware.io.CategoryRequest;
+import in.shani.billingsoftware.io.CategoryResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    CategoryResponse add(CategoryRequest request , MultipartFile file);
+
+    List<CategoryResponse> read();
+
+
+    void delete(String categoryId);
+}
